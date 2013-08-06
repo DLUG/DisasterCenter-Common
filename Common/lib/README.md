8aaac64e7fb54a6d0e4913c5eb7a38ee374f838d
mabin359 <mabin359@gmail.com>
2013-08-06 11:53:16 +0900

    Move APIs from ServerPart

---
+++ Common/lib/README.md
@@ -0,0 +1,1 @@
+Add JSONSimple Library in hear
---
+++ Common/src/org/dlug/disastercenter/common/ApiDaumLocal.java
@@ -0,0 +1,63 @@
+package org.dlug.disastercenter.common;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+public class ApiDaumLocal {
+	private final static String API_URL = "http://apis.daum.net/local/geo/coord2addr";
+	private final static String API_KEY = "DAUM_LOCAL_DEMO_APIKEY";
+	
+	private final static String COORD_SYSTEM = "wgs84";
+	
+	public static String getAddress(double lat, double lng){
+		JSONObject queryResult = getJSON(lat, lng);
+		
+		return queryResult.get("name1") + " " + queryResult.get("name2"); 
+	}
+	
+	private static JSONObject getJSON(double lat, double lng){
+		String requestUrl = API_URL + "?apikey=" + API_KEY 
+				+ "&latitude=" + lat + "&longitude=" + lng 
+				+ "&inputCoordSystem=" + COORD_SYSTEM
+				+ "&output=json";
+		
+		try {
+			URL url;
+			url = new URL(requestUrl);
+			
+			BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
+			
+			String result = "";
+			String inputLine;
+			
+			while ((inputLine = reader.readLine()) != null){
+				result += inputLine;
+			}
+
+			JSONParser parser = new JSONParser();
+			
+			return (JSONObject) parser.parse(result);
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+}
