9ad8287efb9a4c796c6702703ecb10feb03c78b7
mabin359 <mabin359@gmail.com>
2013-08-06 10:48:37 +0900

    Remove Unused Code

--- Common/src/org/dlug/disastercenter/common/CoordinateTools.java
+++ Common/src/org/dlug/disastercenter/common/CoordinateTools.java
@@ -4,10 +4,6 @@ public class CoordinateTools {
 	private final static double LAT_DEGREE_PER_KM = 0.008983;
 	private final static double LNG_DEGREE_PER_KM = 0.015060;
 	
-	private final static double LAT_DEGREE_PER_KMA_Y = 0.045714229;
-	private final static double LNG_DEGREE_PER_KMA_X = 0.052940268;
-	private final static double INCREASE_RATIO_PER_LAT = 0.136325514;
-	
 	private final static int KMA_AMOUNT_X = 149;
 	private final static int KMA_AMOUNT_Y = 253;
 	
@@ -61,32 +57,6 @@ public class CoordinateTools {
 		return longitudeDegree / LNG_DEGREE_PER_KM;
 	}
 
-/*	
-	public static CoordKma latlng2Kma(double latitude, double longitude){
-		double defaultX = longitude - 123.3102;
-		double defaultY = latitude - 31.6518;
-				
-		CoordKma result = new CoordKma();
-		
-		result.x = (int) ((defaultX + (defaultY * INCREASE_RATIO_PER_LAT)) / LNG_DEGREE_PER_KMA_X);
-		result.y = (int) (defaultY / LAT_DEGREE_PER_KMA_Y);
-		
-		return result;
-	}
-	
-	public static CoordLatLng Kma2latlng(int kmaX, int kmaY){
-		double defaultY = kmaY * LAT_DEGREE_PER_KMA_Y;
-		double defaultX = (kmaX * (LNG_DEGREE_PER_KMA_X) - (defaultY * INCREASE_RATIO_PER_LAT));
-				
-		CoordLatLng result = new CoordLatLng();
-		
-		result.lat = defaultY + 31.6518;
-		result.lng = defaultX + 123.3102;
-		
-		return result;
-	}
-*/
-	
 	public static double calcDistance(double srcLat, double srcLng, double dstLat, double dstLng){
 		double degreeDiffLat = Math.asin(dstLat - srcLat);
 		double degreeDiffLng = Math.asin(dstLng - srcLng);
